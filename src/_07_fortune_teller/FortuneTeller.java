e4779e6370e4d1fb728d7e8535c7b8a9f881cdd2
johns <johns@DESKTOP-VF7AOUB>
2020-03-23 15:32:05 -0700

    i did the fortune teller

--- src/_07_fortune_teller/FortuneTeller.java
+++ src/_07_fortune_teller/FortuneTeller.java
@@ -11,9 +11,13 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -24,42 +28,88 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
 
     JFrame frame = new JFrame();
 
-    int frameWidth = 500;
-    int frameHeight = 500;
+    int frameWidth = 400;
+    int frameHeight = 400;
 
     FortuneTeller() throws Exception {
    	 // 1. Choose an image for your fortune teller and put it in your default package
-   	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
+   	 fortuneTellerImage = ImageIO.read(getClass().getResource("kyt_wizard_400.jpg"));
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
      // 3. Complete the begin() method in the FortuneTellerRunner class
  	 // 4. add a mouse listener to the frame
-
+   	 frame.addMouseListener(this);
     }
 
 
 
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public void mousePressed(MouseEvent e) {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-
+   	 if(e.getSource()==frame)
+   		 System.out.println("x is " + mouseX + "y is " + mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 360;
+   	 int secretLocationY = 80;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-
+   		 	String q = JOptionPane.showInputDialog("Any Questions?");
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
-   		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
-   		 // 10. Play the sound
-
+   		// Play a sound
+   		 Clip clip;    		 
+   		 try {
+   		 // 9. You can use the creepy sound we have put in the fortune teller package or
+//   		     put your own spooky sound (.wav) in the package  (freesound.org)
+   		     URL url = getClass().getResource("creepy-noise.wav");
+   		     AudioInputStream audioInputStream =  AudioSystem.getAudioInputStream(url);		
+   		     clip = AudioSystem.getClip(); 
+   		     clip.open(audioInputStream);
+   		     clip.start();
+   		 } catch (Exception e1) {
+   		     // TODO Auto-generated catch block
+   		     System.out.println("cannot get file");
+   		 }
    		 // 11. Use the pause() method below to wait until your music has finished
-
+   		 pause(5);
    		 // 12. Insert your completed Magic 8 ball code here
-
+   		 Random rand = new Random();
+   		 rand.nextInt(8);
+   		 System.out.println(rand);
+   		 String p = JOptionPane.showInputDialog("what number did the future see?");
+   		 
+   		 if(p=="0"){
+   			 System.out.println("um......ok");
+   		 }
+   		 if(p=="1") {
+   			 System.out.println("no u");
+   		 }
+   		 if(p=="2") {
+   			 System.out.println("nobody cares");
+   		 }
+   		 if(p=="3") {
+   			 System.out.println("u will be roblox man");
+   		 }
+   		 if(p=="4") {
+   			 System.out.println("who even writes these");
+   		 }
+   		 if(p=="5") {
+   			 System.out.println("u will be a human");
+   		 }
+   		 if(p=="6") {
+   			 System.out.println("u may be a human");
+   		 }
+   		 if(p=="7") {
+   			 System.out.println("subscribe to tyler wiltz, or else....................... u be no human");
+   		 }
+   		 else {
+   			 System.out.println("haha ur so funny, I almost laughed, wait, subscribe to tylerwiltz");
+   		 }
+   	 
+   	 
    	 }
 
     }
--- src/_07_fortune_teller/FortuneTellerRunner.java
+++ src/_07_fortune_teller/FortuneTellerRunner.java
@@ -1,4 +1,5 @@
 package _07_fortune_teller;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 /*
@@ -14,6 +15,6 @@ public class FortuneTellerRunner {
     
     static void begin() {
       	 // Welcome the user. Give them a hint for the secret location.
-
+    	JOptionPane.showMessageDialog(null, "Hello, human, the wand is probablly where you should go, the bigger the star, the better/just go for the biggest one");
     }
 }
